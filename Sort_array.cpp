78fc631cd1830eaa1a57306bc551609d50c713ac
Aman Chouhan <ak6927887@gmail.com>
2025-09-20 23:33:58 +0530

    Create Sort_array.cpp

---
+++ Sort_array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,9,5,7,4};
+    for(int i=0;i<4;i++){
+        for(int j=i+1;j<=4;j++){
+            if(arr[j]<arr[i]){
+                swap(arr[j],arr[i]);
+            }
+        }
+    }
+    for(int k=0;k<5;k++){
+        cout<<arr[k]<<" ";
+    }
+}
