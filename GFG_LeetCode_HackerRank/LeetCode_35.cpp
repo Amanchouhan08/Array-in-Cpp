228b26e9694c86b0821a763ab9edd7c10df565b2
Aman Chouhan <ak6927887@gmail.com>
2025-09-29 17:16:14 +0530

    Update LeetCode_35.cpp

--- GFG_LeetCode_HackerRank/LeetCode_35.cpp
+++ GFG_LeetCode_HackerRank/LeetCode_35.cpp
@@ -1,1 +1,23 @@
 //Search Insert Position
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int index=nums.size(), start=0, end=nums.size()-1, mid;
+      
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                index=mid;
+                break;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                index=mid;
+                end=mid-1;
+            }
+        }
+        return index;
+    }
+};
