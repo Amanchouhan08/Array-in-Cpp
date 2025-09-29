18f049938a9fe33146da71233336e727516daa20
Aman Chouhan <ak6927887@gmail.com>
2025-09-29 16:17:11 +0530

    Update LeetCode_34.cpp

--- GFG_LeetCode_HackerRank/LeetCode_34.cpp
+++ GFG_LeetCode_HackerRank/LeetCode_34.cpp
@@ -1,1 +1,47 @@
+//Find First and Last Position of Element in Sorted Array.
 
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first=-1, last=-1;
+        int mid,start=0;
+        int end=nums.size()-1;
+
+        //first position
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                first=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+
+        //second position
+        start=0,end=nums.size()-1;
+            while(start<=end){
+            mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                last=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        
+        vector<int>a(2);
+        a[0]=first;
+        a[1]=last;
+
+        return a;
+    }
+};
