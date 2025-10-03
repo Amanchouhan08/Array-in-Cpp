8d82ce223b53c2e1b27aa7610965ba0e8c7abf20
Aman Chouhan <ak6927887@gmail.com>
2025-10-03 16:20:21 +0530

    Update 2D_vector.cpp

--- 2D_vector.cpp
+++ 2D_vector.cpp
@@ -4,6 +4,11 @@
 using namespace std;
 int main(){
   //vector<vector<int>>matrix(row size,vector<int>col size);
+  /*
+    to find the size of row and col
+    int row = matrix.size();
+    int col = matrix[0].size();
+  */
   vector<vector<int>>matrix(3,vector<int>(3));
    for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
