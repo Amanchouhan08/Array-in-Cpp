80a8dae7d6bb4a1b5565c3b289418e643b337894
Aman Chouhan <ak6927887@gmail.com>
2025-10-03 16:26:18 +0530

    Update 2D_vector.cpp

--- 2D_vector.cpp
+++ 2D_vector.cpp
@@ -3,13 +3,20 @@
 #include<algorithm>
 using namespace std;
 int main(){
-  //vector<vector<int>>matrix(row size,vector<int>col size);
+  //vector<vector<int> >matrix(row size,vector<int>col size);
   /*
     to find the size of row and col
     int row = matrix.size();
     int col = matrix[0].size();
   */
-  vector<vector<int>>matrix(3,vector<int>(3));
+  /*
+    vector<vector<int> >matrix(row size,vector<int>col size, initialize);
+    example- vector<vector<int>>matrix(3,vector<int>3, 1);
+    o/p- 1 1 1
+         1 1 1
+         1 1 1
+  */
+  vector<vector<int> >matrix(3,vector<int>(3));
    for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             cin>>matrix[i][j];
